b23379c88d2ef07725af18f22caa84ce9ff9a76a
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-07 14:39:53 -0600

    completed bonus features

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 13-SQLite_create_db_02
 Class exercise where I will create a SQLite database, create tables, insert records to the table, and query the database. We will be using the npm module sqlite3.
+
+
+A link to the requirements for this assignment can be found here:
+https://github.com/nashville-software-school/node-milestones/blob/master/01-foundations/exercises/13-SQLite_create_db_02.md
--- sqlite3.js
+++ sqlite3.js
@@ -11,16 +11,23 @@ const errorHandler = (err)=>{
 }
 
 
-
 // Create a database that is saved on disk.
 
 //create new database
 const db = new sqlite3.Database(`familyBusiness.sqlite`);
 
+//function to drop table if need be
+const dropEmployees = ()=>{
+  db.run(`DROP TABLE employees`, (err)=>{
+    errorHandler(err);
+  })
+}
+//Comment out drop Employees function if not in use
+// dropEmployees()
 // Create a table titled employees with the following columns:
 
 // id, firstName, lastName, jobTitle, address
-db.run(`CREATE TABLE IF NOT EXISTS employees(id, firstName, lastName, jobTitle, address)`, (err) => errorHandler(err));
+db.run(`CREATE TABLE IF NOT EXISTS employees(id INT, firstName TEXT, lastName TEXT, jobTitle TEXT, address TEXT, salary INT)`, (err) => errorHandler(err));
 
 
 // Create an array of at least 6 objects. Each object should have a key value pair matching each column name in the employees table.
@@ -30,28 +37,33 @@ db.run(`CREATE TABLE IF NOT EXISTS employees(id, firstName, lastName, jobTitle, 
 //   ...,
 // ]
 
-let employeeArray = [
-  {id: 1, firstName: 'Jemma', lastName: 'Simmons', jobTitle: 'Scientist', address: 'Secret S.H.E.I.L.D. base' },
-  {id: 2, firstName: 'Phillip', lastName: 'Colson', jobTitle: 'Director', address: 'Secret S.H.E.I.L.D. base' },
-  {id: 3, firstName: 'Leopold', lastName: 'Fitz', jobTitle: 'Scientist', address: 'Secret S.H.E.I.L.D. base' },
-  {id: 4, firstName: 'Daisy', lastName: 'Johnson', jobTitle: 'Agent Quake', address: 'not disclosed' },
-  {id: 5, firstName: 'Melinda', lastName: 'May', jobTitle: 'Field Agent', address: 'on assignment' },
-  {id: 6, firstName: 'Alphonso', lastName: 'Mackenzie', jobTitle: 'Inhuman Director', address: 'Secret S.H.E.I.L.D. base' },
-  {id: 7, firstName: 'Bobbi', lastName: 'Morris', jobTitle: 'Field Agent', address: 'on assignment' },
-  {id: 8, firstName: 'Lance', lastName: 'Hunter', jobTitle: 'Hired Gun', address: 'Secret S.H.E.I.L.D. base' }
-]
+
+//Moved to json file for BONUS challenge
+// let employeeArray = [
+//   {id: 1, firstName: 'Jemma', lastName: 'Simmons', jobTitle: 'Scientist', address: 'Secret S.H.E.I.L.D. base' },
+//   {id: 2, firstName: 'Phillip', lastName: 'Colson', jobTitle: 'Director', address: 'Secret S.H.E.I.L.D. base' },
+//   {id: 3, firstName: 'Leopold', lastName: 'Fitz', jobTitle: 'Scientist', address: 'Secret S.H.E.I.L.D. base' },
+//   {id: 4, firstName: 'Daisy', lastName: 'Johnson', jobTitle: 'Agent Quake', address: 'not disclosed' },
+//   {id: 5, firstName: 'Melinda', lastName: 'May', jobTitle: 'Field Agent', address: 'on assignment' },
+//   {id: 6, firstName: 'Alphonso', lastName: 'Mackenzie', jobTitle: 'Inhuman Director', address: 'Secret S.H.E.I.L.D. base' },
+//   {id: 7, firstName: 'Bobbi', lastName: 'Morris', jobTitle: 'Field Agent', address: 'on assignment' },
+//   {id: 8, firstName: 'Lance', lastName: 'Hunter', jobTitle: 'Hired Gun', address: 'Secret S.H.E.I.L.D. base' }
+// ]
 
 
 // Insert each of the employee objects into the database.
 const populateEmployees = ()=>{
 
-  employeeArray.forEach((each)=>{
+  const {employees} = require(`./employees.json`)
+  console.log("employeses", employees)
+  employees.forEach((each)=>{
     db.run(`INSERT INTO employees VALUES(
       ${each.id},
       "${each.firstName}",
       "${each.lastName}",
       "${each.jobTitle}",
-      "${each.address}")`, (err)=>{
+      "${each.address}",
+      ${each.salary})`, (err)=>{
         errorHandler(err);
       })
 
@@ -76,6 +88,13 @@ db.all(`SELECT jobTitle FROM employees`, (err, allRows)=>{
 
 // Write a statement to query the database and console.log() each employees firstName, lastName and address only.
 db.all(`SELECT firstName || " " || lastName AS Name, address FROM employees`, (err, allRows)=>{
+  errorHandler(err);
+  console.log(allRows);
+});
+
+// Write a statement that returns all employees of a specified jobTitle.
+
+db.all(`SELECT firstName || " " || lastName AS Name, jobTitle FROM employees WHERE jobTitle = 'FieldAgent'`, (err, allRows) =>{
   errorHandler(err);
   console.log(allRows);
 });
